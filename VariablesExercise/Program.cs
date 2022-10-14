b88fb8664710acb4210c570b9c32f9101e287b00
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-10-14 15:49:45 -0400

    completed exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string planetName = "Neptune";
+            int planetAge = 50;
+            char planetSymbol = 'c';
+            bool isInhabitable = false;
+            double distance = 100.52;
+            decimal mass = 30.9m;
+                
+            Console.WriteLine($"My favorite planet is {planetName}. It is about {planetAge} days old. \nI call it {planetSymbol}. Is {planetSymbol} a fun place to be? {isInhabitable}. It's too far. \nIt's like {distance} feet from my house and weighs like {mass} pounds. It's a cool planet.");
+
+
+
+
         }
     }
 }
